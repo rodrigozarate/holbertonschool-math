1145f988e371a2e506a6f52526d0be4cb39e2636
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2021-07-22 16:16:33 -0500

    [CREATE]

---
+++ holberton.h
@@ -0,0 +1,18 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+#include <stdio.h>
+
+/**
+* struct complex - Structure
+*/
+
+typedef struct complex
+{
+	double re;
+	double im;
+} complex;
+
+void display_complex_number(complex c);
+
+#endif
