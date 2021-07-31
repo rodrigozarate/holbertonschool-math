9da9376ebf96686496bf12edcc1bef3245236932
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2021-07-30 23:39:05 -0500

    [CREATE]

---
+++ 0x00-math_complex/6-multiplication.c
@@ -0,0 +1,22 @@
+/*
+* 6-multiplication.c - multiplication of complex
+* Author: Rodrigo Zárate Algecira
+* Date: July 30, 2021
+*/
+
+#include "holberton.h"
+#include <math.h>
+
+/**
+* multiplication - return the multiplication
+* @c1: complex
+* @c2: complex
+* @c3: complex pointer
+* Return: void
+*/
+
+void multiplication(complex c1, complex c2, complex *c3)
+{
+	c3->re = (c1.re * c2.re) - (c1.im * c2.im);
+	c3->im = (c1.re * c2.im) + (c1.im * c2.re);
+}
