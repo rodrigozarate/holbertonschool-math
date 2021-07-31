73efb5854dee922c13ef91f5d84f6b40eb507e2c
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2021-07-30 23:08:04 -0500

    [CREATE]

---
+++ 0x00-math_complex/1-conjugate.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+* conjugate - return the conjugate
+* @c: structure complex
+* Return: complex
+*/
+
+complex conjugate(complex c)
+{
+c.im *= -1;
+return (c);
+}
