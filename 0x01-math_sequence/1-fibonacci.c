d46e78a73cc7fb5b1c62e740114d045c3041de0b
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2021-08-06 21:26:24 -0500

    [CREATE]

---
+++ 0x01-math_sequence/1-fibonacci.c
@@ -0,0 +1,25 @@
+/*
+* 1-fibonacci.c - prints fibonacci secuence
+* Author: Rodrigo Zárate Algecira
+* Date: August 06, 2021
+*/
+
+/**
+* Fibonnaci - Struct fibonacci
+* Return: void
+*/
+
+t_cell *Fibonnaci()
+{
+
+}
+
+/**
+* gold_number - calculate gold number from fibonacci numbers
+* @head: list
+* Return: void
+*/
+double gold_number(t_cell *head)
+{
+
+}
