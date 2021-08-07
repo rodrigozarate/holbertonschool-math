172d759c41dec30633545faaca601216185711db
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2021-08-06 21:08:07 -0500

    [CREATE]

---
+++ 0x01-math_sequence/0-heron.c
@@ -0,0 +1,23 @@
+/*
+* 0-heron.c
+* Author: Rodrigo Zárate Algecira
+* Date: August 06, 2021
+*/
+
+/**
+* heron - Return Heron secuence
+* @p: double
+* @x0: double
+* Return: list
+*/
+
+t_cell *heron(double p, double x0)
+{
+/* recieve params */
+/* create list */
+/* do the math */
+/* iterate over value */
+/* value = 1/2 * ((u->n) + b/(u->n)) */
+/* Populate list */
+/* Return list */
+}
