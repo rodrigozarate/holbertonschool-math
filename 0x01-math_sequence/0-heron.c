f483b213b560969670f8cebce4aa9a3609bd1908
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2021-08-06 22:14:52 -0500

    [UPDATE]

--- 0x01-math_sequence/0-heron.c
+++ 0x01-math_sequence/0-heron.c
@@ -11,16 +11,25 @@
 * Return: list
 */
 
-t_cell *heron(double p, double x0)
+struct t_cell
 {
-/* recieve params */
-/* create list */
-struct t_cell{
 double elt;
 } t_cell;
+
+t_cell *heron(double p, double x0)
+{
+double i = 0;
+double value;
+/* recieve params */
+struct t_cell data;
 /* do the math */
 /* iterate over value */
+for (i = 0; i < x0; i++)
+{
 /* value = 1/2 * ((u->n) + b/(u->n)) */
+}
 /* Populate list */
+data->elt = value;
 /* Return list */
+return (data);
 }
