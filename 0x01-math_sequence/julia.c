521a4ab718a77d66ff266106391a8836bcf9209e
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2021-08-06 21:32:23 -0500

    [CREATE]

---
+++ 0x01-math_sequence/julia.c
@@ -0,0 +1,16 @@
+/*
+* julia.c - Prints julia
+* Author: Rodrigo Zárate Algecira
+* Date: August 06, 2021
+*/
+
+/**
+* julia - do the julia
+*/
+
+julia()
+{
+/* create the julia secuence */
+/* create the array for PGM */
+/* output the PGM file */
+}
