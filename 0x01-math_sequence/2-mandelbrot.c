c31a333a100479ff925547e6af5ce06392a04792
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2021-08-06 21:30:21 -0500

    [CREATE]

---
+++ 0x01-math_sequence/2-mandelbrot.c
@@ -0,0 +1,16 @@
+/*
+* 2-mandelbrot.c - Prints a mandelbrot
+* Author: Rodrigo Zárate Algecira
+* Date: August 06, 2021
+*/
+
+/**
+* mandelbrot - do the mandel bro
+*/
+
+mandelbrot()
+{
+/* create the secuence */
+/* cretae the array for PGM */
+/* output the PGM file */
+}
