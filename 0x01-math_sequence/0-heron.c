e675ec76dc73166418d4de26bd6ba92f1cd9c5fd
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2021-08-08 21:39:48 -0500

    [UPDATE]

--- 0x01-math_sequence/0-heron.c
+++ 0x01-math_sequence/0-heron.c
@@ -4,32 +4,77 @@
 * Date: August 06, 2021
 */
 
+#include "sequences.h"
+
 /**
-* heron - Return Heron secuence
-* @p: double
-* @x0: double
-* Return: list
+* create_node - create node
+* Return: node
 */
 
-struct t_cell
+t_cell *create_node(void)
 {
-double elt;
-} t_cell;
+t_cell *newnode;
+newnode = malloc(sizeof(t_cell));
+if (newnode == NULL)
+	{
+		free(newnode);
+		return (NULL);
+	}
+newnode->next = NULL;
+printf("ss %lf ss", newnode->elt);
+return (newnode);
+}
 
-t_cell *heron(double p, double x0)
+/**
+* add_node - add node
+* @p: pointer
+* @v: double
+* Return: node
+*/
+
+t_cell *add_node(t_cell *p, double v)
 {
+t_cell *tempnode;
+/* tempnode = create_node(); */
+tempnode = p;
+printf("%p p node\n", &p);
+printf("%p tempnode\n", &tempnode);
+tempnode->elt = v;
+printf("%lf tempnode\n", tempnode->elt);
+tempnode->next = p;
+*p = *tempnode;
+return (tempnode);
+}
+
+/**
+* heron - Return Heron secuence
+* @v: double
+* Return: list
+*/
+/* receive params */
+/* t_cell *heron(double p, double x0) */
+t_cell *heron(double v)
+{
+t_cell *thenode, *list;
 double i = 0;
-double value;
-/* recieve params */
-struct t_cell data;
+/* double value = 5; */
+/* double comp = sqr(p); */
+
 /* do the math */
 /* iterate over value */
-for (i = 0; i < x0; i++)
+printf("%f k\n", v);
+/* create node */
+thenode = create_node();
+while (i < v)
 {
-/* value = 1/2 * ((u->n) + b/(u->n)) */
+/* value = 0.5 * (x0 + (p/x0)) */
+/* Populate node list */
+/* thenode = create_node(); */
+printf("%p thenode %lf\n", &thenode, v);
+list = add_node(thenode, v);
+/* printf("%p list\n", &list); */
+v--;
 }
-/* Populate list */
-data->elt = value;
 /* Return list */
-return (data);
+return (list);
 }
