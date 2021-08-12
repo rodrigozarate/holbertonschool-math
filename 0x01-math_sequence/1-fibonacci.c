75996fa311bbc6a5569ccf561bd14e628f3d5c27
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2021-08-12 15:10:12 -0500

    [UPDATE]

--- 0x01-math_sequence/1-fibonacci.c
+++ 0x01-math_sequence/1-fibonacci.c
@@ -11,7 +11,59 @@
 
 t_cell *Fibonnaci()
 {
+/* first two numbers in sequence are 1 and 1 */
+/* Like a seed */
+/* then add the two numbers to get the next */
+/* put a limit by the gold number */
+double goldn = 1.618034;
+int init = 2;
+int pass = 0;
+double togold = 0;
+t_cell *prevnode = NULL;
+t_cell *preprevnode;
+t_cell *tempnode;
 
+tempnode = *prevnode;
+
+/* check if is gold */
+	while (togold != goldn)
+	{
+		if (tempnode == NULL)
+		{
+		printf("tempnode is NULL\n");
+		tempnode = malloc(sizeof(t_cell));
+		/* malloc goes wrong */
+			if (tempnode == NULL)
+				free(tempnode);
+
+		tempnode->elt = 1
+		tempnode->next = NULL;
+
+		}
+		else
+		{
+		printf("tempnode is NOT NULL\n"); 
+		tempnode = malloc(sizeof(t_cell));
+			if (tempnode == NULL)
+				free(tempnode);
+
+			if (togold = 0)
+			{
+			tempnode->elt = 1;
+			tempnode->next = *prevnode;
+			/* never comeback */
+			togold = 1;
+			}
+			else
+			{
+			tempnode->elt = prevnode->elt +;
+
+			}
+		}
+	}
+
+
+return (tempnode);
 }
 
 /**
@@ -21,5 +73,6 @@ t_cell *Fibonnaci()
 */
 double gold_number(t_cell *head)
 {
-
+/* receive a SLL divide the last number with the previous */
+/* Thats it */
 }
