a31c4f511b486fb7c4f9287d288d9c223d5287fc
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2021-08-12 13:40:58 -0500

    [UPDATE]

--- 0x01-math_sequence/0-heron.c
+++ 0x01-math_sequence/0-heron.c
@@ -4,82 +4,68 @@
 * Date: August 06, 2021
 */
 
-#include "sequences.h"
-
-/**
-* create_node - create node
-* Return: node
-*/
-
-t_cell *create_node(void)
-{
-t_cell *newnode;
-newnode = malloc(sizeof(t_cell));
-if (newnode == NULL)
-	{
-		free(newnode);
-		return (NULL);
-	}
-newnode->next = NULL;
-newnode->elt = 74;
-return (newnode);
-}
+#include "heron.h"
 
 /**
 * add_node - add node
-* @p: pointer
+* @tpt: pointer
 * @v: double
 * Return: node
 */
 
-t_cell *add_node(t_cell **p, double v)
+t_cell *add_node(t_cell **tpt, double v)
 {
 t_cell *tempnode;
-tempnode = malloc(sizeof(t_cell));
 
-tempnode->elt = v;
-	if (p[0]->elt == 74)
+tempnode = *tpt;
+
+/* firts time */
+	if (tempnode == NULL)
 	{
-		tempnode->next = NULL;
-		printf("firstnode\n");
-	/*	free(p); */
+	tempnode = malloc(sizeof(t_cell));
+	/* malloc goes wrong */
+		if (tempnode == NULL)
+			free(tempnode);
+
+	tempnode->elt = v;
+	tempnode->next = NULL;
 	}
 	else
 	{
-		tempnode->next = *p;
-		printf("others\n");
-	}
+	tempnode = malloc(sizeof(t_cell));
+		if (tempnode == NULL)
+			free(tempnode);
 
-*p = tempnode;
+	tempnode->elt = v;
+	tempnode->next = *tpt;
+	}
+/*pointer set to new node adress */
+*tpt = tempnode;
+
 return (tempnode);
 }
 
 /**
 * heron - Return Heron secuence
-* @v: double
+* @p: double
+* @x0: double
 * Return: list
 */
 /* receive params */
-/* t_cell *heron(double p, double x0) */
-t_cell *heron(double v)
+t_cell *heron(double p, double x0)
 {
-t_cell *thenode, *list;
-double i = 0;
-/* double comp = sqr(p); */
-
-/* do the math */
-
-/* create node */
-thenode = create_node();
+t_cell *thenode = NULL, *list;
+double value = x0;
+double comp = sqrt(p);
 
 /* iterate over value */
-	for (;i < v; v--)
+	while (comp != value)
 	{
-	/* value = 0.5 * (x0 + (p/x0)) */
+	/* do the math */
+	value = 0.5 * (value + (p / value));
+
 	/* Populate node list */
-
-		list = add_node(&thenode, v);
-
+	list = add_node(&thenode, value);
 	}
 /* Return list */
 return (list);
