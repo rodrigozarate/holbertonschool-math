e2a644cef04945758a319e4e1103fbb5fa8f38d4
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2021-08-16 23:48:12 -0500

    [CREATE] Entry point

---
+++ 0x01-math_sequence/3-main.c
@@ -0,0 +1,35 @@
+/*
+* 3-main.c - Call Julia
+* Author: Rodrigo Zárate Algecira
+* Date: August 16, 2021
+*/
+
+#include "julia.h"
+
+/**
+* main - Entry point for julia
+* @argc: int
+* @argv: char
+* Return: int 0
+*/
+
+int main(int argc, char* argv[])
+{
+	int width, height;
+	complex c;
+ 
+	if(argc != 7)
+		printf("Usage : width, height, real, imaginary, limit radius, iterations");
+	else{
+		width = atoi(argv[1]);
+		height = atoi(argv[2]);
+ 
+		c.x = atof(argv[3]);
+		c.y = atof(argv[4]);
+ 
+		julia(width,height,c,atof(argv[5]),atoi(argv[6]));
+ 
+	}
+ 
+	return 0;
+}
