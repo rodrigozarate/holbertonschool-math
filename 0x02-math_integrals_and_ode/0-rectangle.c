2429339bdb993d6d85310e17670e88700d76ae0d
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2021-08-21 23:24:52 -0500

    [CREATE]

---
+++ 0x02-math_integrals_and_ode/0-rectangle.c
@@ -0,0 +1,18 @@
+/*
+* 0-rectangle.c - rectangle_method
+* Author: Rodrigo Zárate Algecira
+* Date: August 21, 2021
+*/
+
+/**
+* rectangle_method - rectangle_method
+* @a: double
+* @b: double
+* @steps: int
+* Return: double
+*/
+
+double rectangle_method(double a, double b, int steps )
+{
+/* aprox */
+}
