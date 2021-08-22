4d74f0536631efbd3024b6fa8deab04970c1ea96
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2021-08-21 23:27:22 -0500

    [CREATE]

---
+++ 0x02-math_integrals_and_ode/1-simpson.c
@@ -0,0 +1,18 @@
+/*
+* 1-simpson.c - simpson method
+* Author: Rodrigo Zárate Algecira
+* Date: August 21, 2021
+*/
+
+/**
+* rectangle_method - rectangle_method
+* @a: double
+* @b: double
+* @steps: int
+* Return: double
+*/
+
+double simpson(double a, double b, int steps)
+{
+/* aprox */
+}
