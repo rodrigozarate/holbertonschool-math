7b2a03f83581f68954b2fc8289086bc781766df0
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2021-08-26 16:32:34 -0500

    [UPDATE]

--- 0x02-math_integrals_and_ode/0-rectangle.c
+++ 0x02-math_integrals_and_ode/0-rectangle.c
@@ -15,4 +15,25 @@
 double rectangle_method(double a, double b, int steps )
 {
 /* aprox */
+/* b - a / steps = width of rectangle */
+/* area of rectangle = width * height */
+/* sum of rectangles = aprox of area under curve */
+/* height = value of function at the start of the interval */
+/* height = f(a) first case */
+/* height-n = f(a + (i-1) * width) next cases */
+/* area = width * (f(a + (i - 1) * width)) */
+/* create a sum of all areas */
+/* dx = b - a */
+/* 1 + x^2 */
+
+int i;
+int width = (b - a) / steps;
+double sum = 0;
+
+	for (i = 1; i <= steps; i++)
+	{
+		sum += width * (function(a + (i - 1) * width));
+	}
+
+return (sum);
 }
